2d6ad72b4d7c3a1cca81c123d338952390a91184
1AMTHEKID <pascualfrankel@hotmail.com>
2018-12-04 13:51:58 +0100

    Opdrachten aan het updaten.

---
+++ H2/vulmetjs.js
@@ -0,0 +1,4 @@
+document.getElementById("paginatitel").innerText = "hehe, xd";
+document.getElementById("subkop").innerText = "haha, nee.";
+document.getElementById("BigOof1.2").src = "img/BigOof1.2.jpg";
+document.getElementById("BigOof1.3").src = "img/BigOof1.3.jpg";
